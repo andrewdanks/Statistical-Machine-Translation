545dc95fe6853cb098683b35406f0ef09009cb47
Andrew Danks <a.danks@gmail.com>
2012-03-09 04:18:06 -0500

    Fixes to show_analysis

--- A2_SMT/code/show_analysis.m
+++ A2_SMT/code/show_analysis.m
@@ -1,7 +1,9 @@
+
 % Analysis.
+
 function show_analysis(english_sentences, test_sentences)
 
-    total_sentences = length(eng);
+    total_sentences = length(english_sentences);
 
     total_prop = 0;
     for i=1:total_sentences
@@ -9,12 +11,12 @@ function show_analysis(english_sentences, test_sentences)
         test_words = strsplit(' ', test_sentences{i} );
         correct = 0;
         for w=1:min(length(test_words),length(correct_words))
-            if test_words{w} == correct_words{w}
+            if strcmp(test_words{w}, correct_words{w})
                 correct = correct + 1;
             end
         end
         proportion_correct = correct / length(correct_words);
-        disp(['Correct for sentence ', num2str(i), ':', proportion_correct]);
+        disp(['Correct for sentence ', num2str(i), ': ', num2str(proportion_correct)]);
         total_prop = total_prop + proportion_correct;
     end
 
